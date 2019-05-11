49b7aa043a27fff82211fb2043fe29107df95276
BillCipherXANA <mihailo.peki@gmail.com>
2019-05-11 14:44:36 +0200

    new

---
+++ OglasnaTabla/Form1.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Text;
+using System.Windows.Forms;
+
+namespace OglasnaTabla
+{
+    //Treba da se obrati paznja za vanredne casove! Pomnozi trajanje Casova! i saberi sa odmorima! Nadji jednacinu za odmore!!!!
+    public partial class Form1 : Form
+    { 
+        //Test1
+        //DESKTOP VERZIJA
+        int PocH;
+        int PocM;
+
+        public int PocH1;
+        public int PocM1;
+
+        public int PocH2;
+        public int PocM2;
+
+        public int trajanje1;
+        public int trajanje2;
+
+        int trajanje;
+
+        DateTime vreme = DateTime.Now;
+
+        int vremeH;
+
+        int vremeM;
+
+        bool redovan = true;
+
+        //potprogram koji vraća redni broj trenutnog časa, parametri su pocetak nastave (sat i minut), trenutno vreme (sat i minut) i trajanje svakog časa
+
+        public static int redniBrCasa(int pocNastaveH, int poceNastaveM, int vremeH, int vremeM, int trajanjeC, bool prov)
+        {
+            int minutaOdPocetka = vremeH * 60 + vremeM - pocNastaveH * 60 - poceNastaveM;//koliko je minuta proteklo do pocetka nastave
+            if (prov==true)
+            {
+                if ((minutaOdPocetka >= 365) || (minutaOdPocetka < 0))
+                {
+                    return 0;
+                }
+                else if (minutaOdPocetka >= 320)
+                {
+                    return 7;// 7 cas je u toku
+                }
+                else if (minutaOdPocetka >= 270)
+                {
+                    return 6;
+                }
+                else if (minutaOdPocetka >= 220)
+                {
+                    return 5;
+                }
+                else if (minutaOdPocetka >= 165)
+                {
+                    return 4;
+                }
+                else if (minutaOdPocetka >= 115)
+                {
+                    return 3;
+                }
+                else if (minutaOdPocetka >= 50)
+                {
+                    return 2;
+                }
+                else if (minutaOdPocetka >= 0)
+                {
+                    return 1;
+                }
+                return 0;
+            }
+            else
+            {
+               // MessageBox.Show(" " + minutaOdPocetka + trajanjeC);
+                if ((minutaOdPocetka >= ((trajanjeC * 6) + 45 + trajanjeC) || (minutaOdPocetka < 0)))
+                {
+                    return 0;
+                }
+                else if (minutaOdPocetka > ((trajanjeC * 6) + 45))
+                {
+                    return 7;// 7 cas je u toku
+                }
+                else if (minutaOdPocetka >= (trajanjeC * 5) + 40)
+                {
+                    return 6;
+                }
+                else if (minutaOdPocetka >= (trajanjeC * 4) + 35)
+                {
+                    return 5;
+                }
+                else if (minutaOdPocetka >= (trajanjeC * 3) + 30)
+                {
+                    return 4;
+                }
+                else if (minutaOdPocetka >= (trajanjeC * 2) + 10)
+                {
+                    return 3;
+                }
+                else if (minutaOdPocetka >= trajanjeC + 5)
+                {
+                    return 2;
+                }
+                else if (minutaOdPocetka >= 0)
+                {
+                    return 1;
+                }
+                return 0;
+            }
+
+            
+        }
+
+        //potprogram koji vraća koliko minuta je do sledećeg odmora, parametri su pocetak nastave (sat i minut), trenutno vreme (sat i minut) i trajanje svakog časa
+
+        public static int KolikoDoCasa(int pocNastaveH, int poceNastaveM, int vremeH, int vremeM, int trajanjeC, bool provera)
+        {
+            int minutaOdPocetka = vremeH * 60 + vremeM - pocNastaveH * 60 - poceNastaveM;//koliko je minuta proteklo do pocetka nastave
+            if (provera == true)
+            {
+                if ((minutaOdPocetka >= 365) || (minutaOdPocetka < 0))
+                {
+                    return 0;
+                }
+                else if (minutaOdPocetka >= 320)
+                {
+                   
+                    return (365 - 5 - minutaOdPocetka) + 5; //Odmor izmedju 6 i 7 -> 5 min
+                }
+                else if (minutaOdPocetka >= 270)
+                {
+                   
+                    return (320 - 5 - minutaOdPocetka) + 5;  //   Odmor izmedju 5 i 6 -> 5 min
+
+                }
+                else if (minutaOdPocetka >= 220)
+                {
+                    
+                    return (270 - 5 - minutaOdPocetka) + 5;  //  Odmor izmedju 4 i 5 -> 10 min
+                }
+                else if (minutaOdPocetka >= 165)
+                {
+                    
+                    return (220 - 10 - minutaOdPocetka) + 10;  //Odmor izmedju 3 i 4 -> 5 min
+                }
+                else if (minutaOdPocetka >= 115)
+                {
+                   
+                    return (165 - 5 - minutaOdPocetka) + 5;   // Odmor izmedju 2 i 3 -> 20 min
+                }
+                else if (minutaOdPocetka >= 50)
+                {
+                    return (115 - 20 - minutaOdPocetka) + 20;  //Odmor izmedju 1 i 2 -> 5 min.
+                }
+                else if (minutaOdPocetka >= 0)
+                {
+                    return (50 - 5 - minutaOdPocetka) + 5;
+                }
+                return 0;
+            }
+            else
+            {
+                // TESTIRANJE OBAVEZNO!!!
+                if (((minutaOdPocetka >= ((trajanjeC * 6) + 45) + trajanjeC)) || (minutaOdPocetka < 0))
+                {
+                    return 0;
+                }
+                else if (minutaOdPocetka > ((trajanjeC * 6) + 45) && minutaOdPocetka < (((trajanjeC * 6) + 45) + trajanjeC))
+                {
+
+                    return ((trajanjeC * 7 + 50) - minutaOdPocetka); //Odmor izmedju 6 i 7 -> 5 min
+                }
+                else if (minutaOdPocetka >= (trajanjeC * 5) + 40)
+                {
+
+                    return ((trajanjeC * 6 + 45) - minutaOdPocetka);  //   Odmor izmedju 5 i 6 -> 5 min
+
+                }
+                else if (minutaOdPocetka >= (trajanjeC * 4) + 35)
+                {
+
+                    return ((trajanjeC * 5 + 40) - minutaOdPocetka);  //  Odmor izmedju 4 i 5 -> 10 min
+                }
+                else if (minutaOdPocetka >= (trajanjeC * 3) + 30)
+                {
+                    return ((trajanjeC * 4 +35)- minutaOdPocetka);  //Odmor izmedju 3 i 4 -> 20 min
+                }
+                else if (minutaOdPocetka >= (trajanjeC * 2) + 10)
+                {
+                    return ((trajanjeC * 3 +30)- minutaOdPocetka);   // Odmor izmedju 2 i 3 -> 5 min
+                }
+                else if (minutaOdPocetka >= trajanjeC + 5)
+                {
+                    return ((trajanjeC * 2 + 10)-minutaOdPocetka);  //Odmor izmedju 1 i 2 -> 5 min.
+                }
+                else if (minutaOdPocetka >= 0)
+                {
+                    return (trajanjeC + 5) - minutaOdPocetka;
+                }
+            }
+            return 0;
+        }
+        public static int kolikoDoOdmora(int pocNAstaveH, int pocNAstaveM, int vremeH, int vremeM, int trajanje, bool provera)
+        {
+            int minutaodpocetka = vremeH * 60 + vremeM - (pocNAstaveH * 60 + pocNAstaveM);
+            if (provera == true)
+            {
+                // REDOVNI CASOVI
+                if (minutaodpocetka > 365 || minutaodpocetka < 0)
+                {
+                    return 0;
+                }
+                if (minutaodpocetka >= 320 && minutaodpocetka < 365)
+                {
+                    return 365 - minutaodpocetka;//7.cas
+                }
+                if (minutaodpocetka >= 270)
+                {
+                    return 315 - minutaodpocetka;
+                }
+                if (minutaodpocetka >= 220)
+                {
+                    return 265 - minutaodpocetka;
+                }
+                if (minutaodpocetka >= 165)
+                {
+                    return 210 - minutaodpocetka;
+                }
+                if (minutaodpocetka >= 115)
+                {
+                    return 160 - minutaodpocetka;
+                }
+                if (minutaodpocetka >= 50)
+                {
+                    return 95 - minutaodpocetka;
+                }
+                if (minutaodpocetka >= 0)
+                {
+                    return 45 - minutaodpocetka;
+                }
+            }
+            else
+            {
+                // IZMENJENI CASOVI
+                if (minutaodpocetka > 365 || minutaodpocetka < 0)
+                {
+                    return 0;
+                }
+                if (minutaodpocetka > ((trajanje*6)+ 45) && minutaodpocetka < (((trajanje * 6) + 45)+trajanje))
+                {
+                    return ((trajanje * 7) + 45) - minutaodpocetka;//7.cas
+                }
+                if (minutaodpocetka >= (trajanje*5)+40)   // 5-6
+                {
+                    return ((trajanje * 6) + 40) - minutaodpocetka;
+                }
+                if (minutaodpocetka >= (trajanje*4)+35) // 4-5
+                {
+                    return ((trajanje * 5) + 35) - minutaodpocetka;
+                }
+                if (minutaodpocetka >= (trajanje*3)+ 30) //3 - 4
+                {
+                    return ((trajanje * 4) + 30) - minutaodpocetka;
+                }
+                if (minutaodpocetka >= (trajanje*2) + 10)
+                {
+                    return ((trajanje * 3) + 10) - minutaodpocetka; //Prelazak izmedju drugog i treceg casa
+                }
+                if (minutaodpocetka >= trajanje+5)
+                {
+                    return ((trajanje*2)+5) - minutaodpocetka;
+                }
+                if (minutaodpocetka >= 0)
+                {
+                    return trajanje - minutaodpocetka;
+                }
+            }
+            // Izmenjeno
+            
+            return 0;
+        }
+       
+        public Form1()
+        {
+           
+            
+            InitializeComponent();
+            label1.BackColor = System.Drawing.Color.Transparent;
+            label2.BackColor = System.Drawing.Color.Transparent;
+            label3.BackColor = System.Drawing.Color.Transparent;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {  
+            vreme = DateTime.Now;
+            vremeH = vreme.Hour;
+            vremeM = vreme.Minute;
+            
+            if (redovan == true)
+            {
+                if (vremeH < 14)
+                {
+                    PocH = 7;
+                    PocM = 45;
+                    trajanje = 45;
+                }
+                if ((vremeH >= 14)&&(vremeH<21))
+                {
+                    PocH = 14;
+                    PocM = 0;
+                    trajanje = 45;
+                }
+                if (kolikoDoOdmora(PocH, PocM, vremeH, vremeM, trajanje, redovan) > 0)
+                {
+                    label2.Left = (this.Size.Width - label2.Size.Width) / 2;
+                    label2.Text = "U toku je " + redniBrCasa(PocH, PocM, vremeH, vremeM, trajanje, redovan) + ". ^as, molimo Vas za ti[inu. ^as se zavr[ava za " + kolikoDoOdmora(PocH, PocM, vremeH, vremeM, trajanje, redovan) + ". minuta";
+                }
+                else
+                {
+                    label2.Left = (this.Size.Width - label2.Size.Width) / 2;
+                    label2.Text = (redniBrCasa(PocH, PocM, vremeH, vremeM, trajanje,redovan) + 1) + ". ^as po^iwe za " + (KolikoDoCasa(PocH, PocM, vremeH, vremeM, trajanje, redovan) + ". minuta! U@ivajte u odmoru!");
+                    
+                    
+                }
+            }
+            else
+            {
+                if ((vremeH < PocH1)||((vremeH<PocH2)&&(vremeH>PocH1)) || ((vremeH == PocH1) && (vremeM >= PocM1)))
+                {
+                   
+                    PocH = PocH1;
+                    PocM = PocM1;
+                    trajanje = trajanje1;
+                  //  MessageBox.Show("Prva smena> " + " " + PocH + " " + PocM + " " + trajanje);
+                }
+                else if ((vremeH > PocH2) || ((vremeH == PocH2)&&(vremeM>=PocM2)))
+                {
+                    PocH = PocH2;
+                    PocM = PocM2;
+                    trajanje = trajanje2;
+                   // MessageBox.Show("Druga smena> " + " " + PocH + " " + PocM + " " + trajanje);
+                }
+                if (kolikoDoOdmora(PocH, PocM, vremeH, vremeM, trajanje, redovan) > 0)
+                {
+                    label2.Text = "U toku je " + (redniBrCasa(PocH, PocM, vremeH, vremeM, trajanje, redovan)) + ". ^as, molimo Vas za ti[inu. ^as se zavr[ava za " + kolikoDoOdmora(PocH, PocM, vremeH, vremeM, trajanje, redovan) + ". minuta";
+                    label2.Left = (this.Size.Width - label2.Size.Width) / 2;
+                }
+                else
+                {
+                    label2.Left = (this.Size.Width - label2.Size.Width) /2 ;
+                    
+                    label2.Text = (redniBrCasa(PocH, PocM, vremeH, vremeM, trajanje, redovan) + 1) + ". ^as po^iwe za " + (KolikoDoCasa(PocH, PocM, vremeH, vremeM, trajanje, redovan) + ". minuta! U@ivajte u odmoru!");
+                }
+            }
+            label3.Text = vreme.Date.ToLongDateString();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Fpodesavanje podesi = new Fpodesavanje();
+            if (podesi.ShowDialog() == DialogResult.OK)
+            {
+                redovan = podesi.redovan;
+                if (redovan==false)
+                {
+                    PocH1 = podesi.pocH1;
+                    PocM1 = podesi.pocM1;
+                    PocH2 = podesi.pocH2;
+                    PocM2 = podesi.pocM2;
+                    trajanje1 = podesi.trajanje1;
+                    trajanje2 = podesi.trajanje2;
+                    
+                }
+            }
+            //MessageBox.Show("Test> " + PocH1 + " " + PocM1 + " " + PocH2 + " "+ PocM2 + " " +trajanje1 +" "+ trajanje2 + redovan);
+           
+        }
+
+       
+    }
+}
---
+++ OglasnaTabla/Fpodesavanje.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace OglasnaTabla
+{
+    partial class Fpodesavanje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Fpodesavanje));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.Font = new System.Drawing.Font("Beograd", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(168, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PODE[AVAWE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(386, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Прва смена";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 107);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(112, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Редовна настава";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(255, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Почетак наставе (h)";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(369, 118);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(118, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "Ванредна настава";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(369, 154);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(369, 180);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(245, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Почетак наставе (min)";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(369, 206);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(269, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Трајање наставе";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(369, 317);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(269, 320);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Трајање наставе";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(369, 291);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(245, 294);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Почетак наставе (min)";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(369, 265);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(255, 268);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(108, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Почетак наставе (h)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(386, 249);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(73, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Друга смена";
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(150, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 41);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Сачувај";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Fpodesavanje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(499, 352);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Fpodesavanje";
+            this.Text = "Fpodesavanje";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
